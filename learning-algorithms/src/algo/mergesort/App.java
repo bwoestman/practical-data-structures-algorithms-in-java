da6c35a5217d363c4a75ea54c1a8eb5009d7357b
Brian Woestman <bwoestman@widen.com>
2021-08-12 23:35:29 -0500

    finish up merge sort

--- learning-algorithms/src/algo/mergesort/App.java
+++ learning-algorithms/src/algo/mergesort/App.java
@@ -5,13 +5,11 @@ public class App
     public static void main(String[] args)
     {
         int[] inputArray = {9,7,3,1,6,3,2,6,8,9,2,3,0};
-        MergeSort sorter = new MergeSort();
 
-        sorter.sort(inputArray);
+        MergeSort.sort(inputArray);
 
-        for(int i = 0; i < inputArray.length; i++)
-        {
-            System.out.println(inputArray[i]);
+        for (int j : inputArray) {
+            System.out.println(j);
         }
     }
 }
--- learning-algorithms/src/algo/mergesort/MergeSort.java
+++ learning-algorithms/src/algo/mergesort/MergeSort.java
@@ -1,29 +1,55 @@
 package algo.mergesort;
 
-import java.util.Arrays;
-
-public class MergeSort
-{
-    public static void sort(int inputArray[])
-    {
+public class MergeSort {
+    public static void sort(int[] inputArray) {
         sort(inputArray, 0, inputArray.length - 1);
     }
 
-    public static void sort(int inputArray[], int start, int end)
-    {
-        if (end <= start)
-        {
+    public static void sort(int[] inputArray, int start, int end) {
+        if (start >= end) {
             return;
         }
         int mid = (start + end) / 2;
+
         sort(inputArray, start, mid);
         sort(inputArray, mid + 1, end);
+
         merge(inputArray, start, mid, end);
     }
 
-    public static void merge(int inputArray[], int start, int mid, int end)
-    {
-        int[] front = Arrays.copyOfRange(inputArray, 0, mid);
-        int[] back = Arrays.copyOfRange(inputArray, mid + 1, end);
+    public static void merge(int[] inputArray, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int k = 0;
+
+        int left = start;
+        int right = mid + 1;
+
+        while(left <= mid && right <= end) {
+            if (inputArray[left] < inputArray[right]) {
+                temp[k] = inputArray[left];
+                left++;
+            } else {
+                temp[k] = inputArray[right];
+                right++;;
+            }
+            k++;
+        }
+
+        while (left <= mid) {
+            temp[k] = inputArray[left];
+            left++;
+            k++;
+        }
+
+        while (right <= end) {
+            temp[k] = inputArray[right];
+            right++;
+            k++;
+        }
+
+        for (int j : temp) {
+            inputArray[start] = j;
+            start++;
+        }
     }
 }
